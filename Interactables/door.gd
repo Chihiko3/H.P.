0ce52a356aac877a47c80887dc39fedc9c2d4044
Chihiko3 <89134046+Chihiko3@users.noreply.github.com>
2023-09-29 02:50:19 +0800

    door?

---
+++ Interactables/door.gd
@@ -0,0 +1,26 @@
+extends Interactable
+@onready var animation_player = $AnimationPlayer
+
+var isOpen := false
+var canInteract := true
+
+func action_use():
+	if canInteract:
+		if isOpen:
+			close()
+		else:
+			open()
+	
+func close():
+	animation_player.play("door_close")
+	isOpen = false
+	canInteract = false
+
+		
+func open():
+	animation_player.play("door_open")
+	isOpen = true
+	canInteract = false
+	
+func _on_animation_player_animation_finished(anim_name):
+	canInteract = true
