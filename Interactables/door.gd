8da808be5467161a036405a300f81db37c63184f
Chihiko3 <89134046+Chihiko3@users.noreply.github.com>
2023-09-29 18:36:35 +0800

    door

--- Interactables/door.gd
+++ Interactables/door.gd
@@ -3,13 +3,27 @@ extends Interactable
 
 var isOpen := false
 var canInteract := true
+@export var isLocked := false
+@export_node_path("Area3D") var keyPath
+var actualKey
+
+func _ready():
+	if keyPath != null:
+		actualKey = get_node(keyPath)
 
 func action_use():
-	if canInteract:
-		if isOpen:
-			close()
-		else:
-			open()
+	if isLocked and !is_instance_valid(actualKey):
+		isLocked = false
+		print("Door unlocked")
+	
+	if !isLocked:
+		if canInteract:
+			if isOpen:
+				close()
+			else:
+				open()
+	else:
+		animation_player.play("locked_door")
 	
 func close():
 	animation_player.play("door_close")
